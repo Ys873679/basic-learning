97815cd3686a361ca2bedf695e01a544e4a64b98
ys873679 <ys873679@gmail.com>
2025-05-23 18:31:50 +0530

    inital code setup done

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,16 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Ready to create an app?</strong>
+      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione doloribus suscipit quam temporibus possimus aliquid autem repudiandae architecto accusantium. Nesciunt!</h1>
+    </div>
+  );
+};
+
+export default ExploreContainer;
